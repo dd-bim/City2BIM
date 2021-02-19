720a204ac4e1cdaed7add77e2a20e540ca79a131
Marcus Schröder <marcus.schroeder2@htw-dresden.de>
2021-02-19 15:01:03 +0100

    ADD for ifc4 processing

---
+++ BIMGISInteropLibs/IFC/Ifc4/InitModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//embed BimGisCad
+using BimGisCad.Representation.Geometry;
+using BimGisCad.Representation.Geometry.Elementary;
+using BimGisCad.Representation.Geometry.Composed;
+using BimGisCad.Collections;                    //provides MESH --> will be removed
+
+//embed Xbim                                    //below selected examples that show why these are included
+using Xbim.Ifc;                                 //IfcStore
+using Xbim.Ifc4.GeometricConstraintResource;    //IfcLocalPlacement
+using Xbim.Ifc4.GeometryResource;               //IfcAxis2Placement3D
+using Xbim.Ifc4.Kernel;                         //IfcProject
+using Xbim.Common.Step21;                       //Enumeration to XbimShemaVersion
+using Xbim.IO;                                  //Enumeration to XbimStoreType
+using Xbim.Common;                              //ProjectUnits (Hint: support imperial (TODO: check if required)
+using Xbim.Ifc4.Interfaces;                     //Enumeration for Unit
+using Xbim.Ifc4.ProductExtension;               //IfcSite
+using Xbim.Ifc4.GeometricModelResource;         //IfcShellBasedSurfaceModel or IfcGeometricCurveSet
+using Xbim.Ifc4.TopologyResource;               //IfcOpenShell
+using Xbim.Ifc4.RepresentationResource;         //IfcShapeRepresentation
+
+namespace BIMGISInteropLibs.IFC.Ifc4
+{
+    public static class InitModel
+    {
+        /// <summary>
+        /// Initializes an empty project
+        /// </summary>
+        /// <param name="projectName">Titel of project</param>
+        /// <param name="editorsFamilyName">family name</param>
+        /// <param name="editorsGivenName">given name</param>
+        /// <param name="editorsOrganisationName">organisation</param>
+        /// <param name="project">Return parameter for further processing</param>
+        /// <returns>IfcProject</returns>
+        public static IfcStore Create(string projectName,
+            string editorsFamilyName,
+            string editorsGivenName,
+            string editorsOrganisationName,
+            out IfcProject project)
+        {
+            //first we need to set up some credentials for ownership of data in the new model
+            var credentials = new XbimEditorCredentials
+            {
+                ApplicationDevelopersName = "HTW Dresden for DDBIM",
+                ApplicationFullName = System.Reflection.Assembly.GetExecutingAssembly().FullName,
+                ApplicationIdentifier = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name,
+                ApplicationVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(),
+                EditorsFamilyName = editorsFamilyName,
+                EditorsGivenName = editorsGivenName,
+                EditorsOrganisationName = editorsOrganisationName
+            };
+            //write credentials to IfcStore (model)
+            var model = IfcStore.Create(credentials, XbimSchemaVersion.Ifc2X3, XbimStoreType.EsentDatabase);
+
+            //Begin a transaction as all changes to a model are ACID
+            using (var txn = model.BeginTransaction("Initialise Model"))
+            {
+                //create a project
+                project = model.Instances.New<IfcProject>();
+
+                //set the units to SI (metres)
+                project.Initialize(ProjectUnits.SIUnitsUK);
+                
+                //set project Name
+                project.Name = projectName;
+
+                project.UnitsInContext.SetOrChangeSiUnit(IfcUnitEnum.LENGTHUNIT, IfcSIUnitName.METRE, null);
+                //now commit the changes, else they will be rolled back at the end of the scope of the using statement
+                txn.Commit();
+            }
+            return model;
+        }
+    }
+}
--- BIMGISInteropLibs/IFC/Ifc4/Model.cs
+++ BIMGISInteropLibs/IFC/Ifc4/Model.cs
@@ -4,9 +4,93 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//embed BimGisCad
+using BimGisCad.Representation.Geometry;
+using BimGisCad.Representation.Geometry.Elementary;
+using BimGisCad.Representation.Geometry.Composed;
+using BimGisCad.Collections;                    //provides MESH --> will be removed
+
+//embed Xbim                                    //below selected examples that show why these are included
+using Xbim.Ifc;                                 //IfcStore
+using Xbim.Ifc4.GeometricConstraintResource;    //IfcLocalPlacement
+using Xbim.Ifc4.GeometryResource;               //IfcAxis2Placement3D
+using Xbim.Ifc4.Kernel;                         //IfcProject
+using Xbim.Common.Step21;                       //Enumeration to XbimShemaVersion
+using Xbim.IO;                                  //Enumeration to XbimStoreType
+using Xbim.Common;                              //ProjectUnits (Hint: support imperial (TODO: check if required)
+using Xbim.Ifc4.Interfaces;                     //Enumeration for Unit
+using Xbim.Ifc4.MeasureResource;                //IfcLabel
+using Xbim.Ifc4.ProductExtension;               //IfcSite
+using Xbim.Ifc4.GeometricModelResource;         //IfcShellBasedSurfaceModel or IfcGeometricCurveSet
+using Xbim.Ifc4.TopologyResource;               //IfcOpenShell
+using Xbim.Ifc4.RepresentationResource;         //IfcShapeRepresentation
+
+
 namespace BIMGISInteropLibs.IFC.Ifc4
 {
     class Model
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="projectName"></param>
+        /// <param name="editorsFamilyName"></param>
+        /// <param name="editorsGivenName"></param>
+        /// <param name="editorsOrganisationName"></param>
+        /// <param name="siteName"></param>
+        /// <param name="sitePlacement"></param>
+        /// <param name="tin"></param>
+        /// <param name="surfaceType"></param>
+        /// <param name="breakDist"></param>
+        /// <param name="refLatitude"></param>
+        /// <param name="refLongitude"></param>
+        /// <param name="refElevation"></param>
+        /// <returns></returns>
+        public static IfcStore CreateViaTin(
+            string projectName,
+            string editorsFamilyName,
+            string editorsGivenName,
+            string editorsOrganisationName,
+            IfcLabel siteName,
+            Axis2Placement3D sitePlacement,
+            Tin tin,
+            SurfaceType surfaceType,
+            double? breakDist = null,
+             double? refLatitude = null,
+             double? refLongitude = null,
+             double? refElevation = null)
+        {
+            var model = InitModel.Create(projectName, editorsFamilyName, editorsGivenName, editorsOrganisationName, out var project);
+            var site = Site.Create(model, siteName, sitePlacement, refLatitude, refLongitude, refElevation);
+            RepresentationType representationType;
+            RepresentationIdentifier representationIdentifier;
+            IfcGeometricRepresentationItem shape;
+            switch (surfaceType)
+            {
+                case SurfaceType.TFS:
+                    return null;
+                case SurfaceType.SBSM:
+                    shape = ShellBasedSurfaceModel.CreateViaTin(model, sitePlacement.Location, tin, out representationType, out representationIdentifier);
+                    break;
+                default:
+                    shape = GeometricCurveSet.CreateViaTin(model, sitePlacement.Location, tin, breakDist, out representationType, out representationIdentifier);
+                    break;
+            }
+            var repres = ShapeRepresentation.Create(model, shape, representationIdentifier, representationType);
+
+            using (var txn = model.BeginTransaction("Add Site to Project"))
+            {
+                site.Representation = model.Instances.New<IfcProductDefinitionShape>(r => r.Representations.Add(repres));
+                project.AddSite(site);
+
+                model.OwnerHistoryAddObject.CreationDate = DateTime.Now;
+                model.OwnerHistoryAddObject.LastModifiedDate = model.OwnerHistoryAddObject.CreationDate;
+
+                txn.Commit();
+            }
+
+            return model;
+        }
+
     }
 }
