13b929fb2be598cfebde7d9346a127fb8d87b603
Marcus Schröder <marcus.schroeder2@htw-dresden.de>
2021-02-22 09:46:09 +0100

    backup GUI

--- IFCTerrainGUI/GUI/IfcTerrainGUI.cs
+++ IFCTerrainGUI/GUI/IfcTerrainGUI.cs
@@ -11,6 +11,9 @@ using System.Windows.Forms;
 using System.IO; //requiered for file handling
 
 
+using BIMGISInteropLibs.IfcTerrain; //so that functionalities like ConnectionInterface, Result, JsonSeetings are available!
+
+
 namespace IFCTerrainGUI
 {
     public partial class IfcTerrainGUI : Form
@@ -21,20 +24,31 @@ namespace IFCTerrainGUI
         {
             InitializeComponent();
         }
+        /// <summary>
+        /// JSON (settings) - passing information (will be necessary for further processing)
+        /// </summary>
+        public JsonSettings jSettings { get; set; } = new JsonSettings();
+
+
+
 
         #region dxf processing
 
         private void btnReadDxf_Click(object sender, EventArgs e)
         {
 
+
+
             var ofd = new OpenFileDialog
             {
                 Filter = "DXF Files *.dxf, *.dxb|*.dxf;*.dxb"
             };
+            //if a file was selected via the filter (above) 
             if (ofd.ShowDialog() == DialogResult.OK)
             {
-
+                
             }
+            //[TODO]: otherwise there have to be an error massage or hint
         }
 
 
