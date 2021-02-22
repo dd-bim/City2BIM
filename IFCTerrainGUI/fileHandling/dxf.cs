946acd50fb2259201eb62ed9b8bc6393faf77ca7
Marcus Schröder <marcus.schroeder2@htw-dresden.de>
2021-02-22 18:09:01 +0100

    update handling for dxf file

--- IFCTerrainGUI/fileHandling/dxf.cs
+++ IFCTerrainGUI/fileHandling/dxf.cs
@@ -6,14 +6,18 @@ using System.Threading.Tasks;
 
 using System.Windows.Forms; //File Dialog
 
-using BIMGISInteropLibs.IfcTerrain; 
+using BIMGISInteropLibs.IfcTerrain;
+
+using System.ComponentModel; //include to be able to address background workers
+
+using IxMilia.Dxf;  //include to be able to process DXF files
 
 namespace IFCTerrainGUI.fileHandling
 {
     /// <summary>
     /// Class to support processing (via the GUI)
     /// </summary>
-    public class dxf
+    public static class dxf
     {
         /// <summary>
         /// DXF file opening; if a file was opened successfully, Json settings are made.
@@ -22,15 +26,40 @@ namespace IFCTerrainGUI.fileHandling
         /// <param name="e"></param>
         public static void openDxf(object sender, EventArgs e)
         {
+            //Dialog for opening a file
             var ofd = new OpenFileDialog
             {
+                //preseting a filter to open dxf files
                 Filter = "DXF Files *.dxf, *.dxb|*.dxf;*.dxb"
             };
+
             //if a file was selected via the filter (above) 
             if (ofd.ShowDialog() == DialogResult.OK)
             {
+                #region json settings
+                //set file type to dxf
                 IfcTerrainGUI.jSettings.fileType = IfcTerrainFileType.Dxf;
-                MessageBox.Show("DEV TEST");
+
+                //stoarge locaction setzen
+                IfcTerrainGUI.jSettings.filePath = ofd.FileName;
+                #endregion json settings
+
+                #region gui text messages
+                //placeholder
+                #endregion gui text messages
+
+                #region background worker async
+                //background worker start: this lists all DXF entries (from the DXF file) in the ListBox
+                IfcTerrainGUI.backgroundWorkerDxf.RunWorkerAsync(ofd.FileName);
+                #endregion background worker async
+
+                #region error handling
+                //placeholder for error handling
+                #endregion error handling
+
+                #region logging
+                //placeholder
+                #endregion logging
             }
             //[TODO]: otherwise there have to be an error massage or hint
 
