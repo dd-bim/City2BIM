d29cd6574c233998f85f5732190bafad686182d1
Marcus Schröder <marcus.schroeder2@htw-dresden.de>
2021-02-23 19:13:08 +0100

    backup WPF IFCTerrain

--- IFCTerrainGUI_WPF/MainWindow.xaml.cs
+++ IFCTerrainGUI_WPF/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace IFCTerrainGUI_WPF
+namespace IFCTerrainGUI
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
