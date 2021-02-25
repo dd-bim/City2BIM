a88ff9a60540e7be0fa6fb99a9836249fb6848e8
Marcus Schröder <marcus.schroeder2@htw-dresden.de>
2021-02-25 18:17:54 +0100

    update gui

--- IFCTerrainGUI/MainWindow.xaml.cs
+++ IFCTerrainGUI/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-
+using IFCTerrainGUI.GUI.MainWindowLogic; //used to outsource auxiliary functions
 
 //integrate logic from BIMGISInteropsLibs 
 using BIMGISInteropLibs.IfcTerrain; //used for JsonSettings, ...
@@ -40,19 +40,8 @@ namespace IFCTerrainGUI
         /// </summary>
         public static JsonSettings jSettings { get; set; } = new JsonSettings();
 
-        /// <summary>
-        /// Function to assign text to a textbox;
-        /// Note: this scrolls to the end of the text box so that the end of the file path is displayed
-        /// </summary>
-        /// <param name="tbName">Text box identifier</param>
-        /// <param name="tbText">Text which should be used</param>
-        public static void setTextBoxText(TextBox tbName, string tbText)
-        {
-            tbName.Text = tbText;
-            tbName.CaretIndex = tbText.Length;
-            var rect = tbName.GetRectFromCharacterIndex(tbName.CaretIndex);
-            tbName.ScrollToHorizontalOffset(rect.Right);
-        }
+        
+  
 
 
         /// <summary>
@@ -62,21 +51,7 @@ namespace IFCTerrainGUI
         /// <param name="e"></param>
         private void btnTest_Click(object sender, RoutedEventArgs e)
         {
-            setTextBoxText(tbFilePathInput, jSettings.filePath);
-        }
-
-        /// <summary>
-        /// take over the properties, from the read XML file
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void btnProcessXml_Click(object sender, RoutedEventArgs e)
-        {
-            setTextBoxText(tbFilePathInput, jSettings.filePath);
-            setTextBoxText(tbFileTypeInput, jSettings.fileType.ToString());
-            setTextBoxText(tbDtmLayerInput, "will not be read out at " + jSettings.fileType.ToString());
-            setTextBoxText(tbGridSizeInput, "will not be read out at " + jSettings.fileType.ToString());
-
+            //MainWindowBib.setTextBoxText(tbFilePathInput, jSettings.filePath);
         }
     }
 }
