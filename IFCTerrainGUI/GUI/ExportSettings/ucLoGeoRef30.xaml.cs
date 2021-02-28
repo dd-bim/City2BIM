c6c0a14c1f303393f0abd8ebc094504255950ac3
Marcus Schröder <marcus.schroeder2@htw-dresden.de>
2021-02-28 17:49:49 +0100

    add user controler for LoGeoRef30

---
+++ IFCTerrainGUI/GUI/ExportSettings/ucLoGeoRef30.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IFCTerrainGUI.GUI.ExportSettings
+{
+    /// <summary>
+    /// Interaktionslogik für ucLoGeoRef30.xaml
+    /// </summary>
+    public partial class ucLoGeoRef30 : UserControl
+    {
+        public ucLoGeoRef30()
+        {
+            InitializeComponent();
+        }
+    }
+}
