9a2540e54fa69e31e250d4d70617e6ad87ab9f4e
Tim <tim.kaiser@htw-dresden.de>
2021-02-28 14:06:56 +0100

    add spatial filter options in xplanimportGUI

--- City2RVT/GUI/XPlan2BIM/ImportXPlanDialog.xaml.cs
+++ City2RVT/GUI/XPlan2BIM/ImportXPlanDialog.xaml.cs
@@ -6,8 +6,10 @@ using System.Windows;
 using Microsoft.Win32;
 
 using Autodesk.Revit.UI;
+using Serilog;
 
 using BIMGISInteropLibs.XPlanung;
+using BIMGISInteropLibs.OGR;
 using City2RVT.GUI.Modify;
 
 namespace City2RVT.GUI.XPlan2BIM
@@ -17,6 +19,7 @@ namespace City2RVT.GUI.XPlan2BIM
     /// </summary>
     public partial class ImportXPlanDialog : Window
     {
+        private Autodesk.Revit.DB.Document doc { get; set; }
         private List<LayerStatus> layerStatusList { get; set; }
         private bool startImport { set; get; }
         public bool StartImport
@@ -52,7 +55,11 @@ namespace City2RVT.GUI.XPlan2BIM
             }
         }
 
-        public ImportXPlanDialog(bool terrainAvailable)
+        public OGRSpatialFilter SpatialFilter { get => spatialFilter; }
+
+        private OGRSpatialFilter spatialFilter { get; set; }
+
+        public ImportXPlanDialog(bool terrainAvailable, Autodesk.Revit.DB.Document doc)
         {
             this.layerStatusList = new List<LayerStatus>();
             InitializeComponent();
@@ -76,27 +83,31 @@ namespace City2RVT.GUI.XPlan2BIM
                 {
                     filePathBox.Text = openFileDialog.FileName;
 
-                    XDocument xmlDoc = XDocument.Load(openFileDialog.FileName);
+                    //XDocument xmlDoc = XDocument.Load(openFileDialog.FileName);
 
-                    XPlanungReader xPlanReader = new XPlanungReader(xmlDoc);
-                    xPlanReader.readData();
+                    //XPlanungReader xPlanReader = new XPlanungReader(xmlDoc);
+                    //xPlanReader.readData();
 
-                    var xPlanObjs = xPlanReader.XPlanungObjects;
+                    //var xPlanObjs = xPlanReader.XPlanungObjects;
 
-                    var avialableLayers = from xObj in xPlanObjs
-                                          group xObj by xObj.UsageType into usageGroup
-                                          select usageGroup;
+                    //var avialableLayers = from xObj in xPlanObjs
+                    //                      group xObj by xObj.UsageType into usageGroup
+                    //                      select usageGroup;
+
+                    var gmlReader = new OGRGMLReader(openFileDialog.FileName);
 
                     List<LayerStatus> layerStatusList = new List<LayerStatus>();
 
-                    foreach (var usageGroup in avialableLayers)
+                    foreach (var usageGroup in gmlReader.getLayerList())
                     {
-                        layerStatusList.Add(new LayerStatus { LayerName = usageGroup.Key, Visibility = true });
+                        layerStatusList.Add(new LayerStatus { LayerName = usageGroup, Visibility = true });
                     }
 
                     this.layerStatusList = layerStatusList;
                     LayerTable.ItemsSource = this.layerStatusList;
-                
+
+                    gmlReader.destroy();
+
                 } catch (Exception ex)
                 {
                     TaskDialog.Show("Error!", "An error occured. Did you specify a valid XPlan-File?" + "\n\n" + ex.ToString());
@@ -131,13 +142,43 @@ namespace City2RVT.GUI.XPlan2BIM
             if (this.layerNamesToImport.Count > 0)
             {
                 this.startImport = true;
-                if (drapeCheckBox.IsEnabled & (bool) drapeCheckBox.IsChecked)
+                if (drapeCheckBox.IsEnabled & (bool)drapeCheckBox.IsChecked)
                 {
                     this.drape = true;
                 }
                 else
                 {
                     this.drape = false;
+                }
+
+                if (this.filterCheckBox.IsChecked == true)
+                {
+                    var pbp = utils.getProjectBasePointMeter(doc);
+                    double distance;
+
+                    try
+                    {
+                        distance = double.Parse(distanceTextBox.Text);
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Error("Cannot parse value for spatial filter \n" + ex.ToString());
+                        TaskDialog.Show("Error", "Is value for distance/radius a number?");
+                        return;
+                    }
+
+                    if (filterTypeBox.SelectedValue.ToString() == "Circle")
+                    {
+                        this.spatialFilter = new OGRCircularSpatialFilter(pbp.X, pbp.Y, distance);
+                    }
+                    else if (filterTypeBox.SelectedValue.ToString() == "Square")
+                    {
+                        this.spatialFilter = new OGRRectangularSpatialFilter(pbp.X, pbp.Y, distance, distance);
+                    }
+                }
+                else
+                {
+                    this.spatialFilter = null;
                 }
 
                 this.filePath = filePathBox.Text;
