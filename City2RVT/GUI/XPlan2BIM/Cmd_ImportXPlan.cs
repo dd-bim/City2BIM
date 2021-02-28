8efcb9c2f742ca5234cec9595077ab4c2bb4eaf1
Tim <tim.kaiser@htw-dresden.de>
2021-02-28 14:05:31 +0100

    use new ogrreader in xplan import

--- City2RVT/GUI/XPlan2BIM/Cmd_ImportXPlan.cs
+++ City2RVT/GUI/XPlan2BIM/Cmd_ImportXPlan.cs
@@ -12,6 +12,8 @@ using System.Xml.Linq;
 using BIMGISInteropLibs.XPlanung;
 using City2RVT.Builder;
 using City2RVT.Reader;
+using BIMGISInteropLibs.OGR;
+using Serilog;
 
 using City2RVT.GUI.XPlan2BIM;
 
@@ -30,31 +32,55 @@ namespace City2RVT.GUI
             UIDocument uiDoc = revit.Application.ActiveUIDocument;
             Document doc = uiDoc.Document;
             Prop_GeoRefSettings.SetInitialSettings(doc);
-            MetaInformation.createXPlanungSchema(doc);
+            //MetaInformation.createXPlanungSchema(doc);
 
             ElementId terrainId = utils.getHTWDDTerrainID(doc);
 
             // if a base dtm is loaded then terrainAvailable => true
             bool terrainAvailable = (terrainId != null) ? true : false;
 
-            var dialog = new ImportXPlanDialog(terrainAvailable);
+            if (!utils.configureOgr())
+            {
+                Log.Error("Could not configure GDAL/OGR");
+                TaskDialog.Show("Error", "Could not configure GDAL/OGR");
+                return Result.Failed;
+            }
+
+            var dialog = new ImportXPlanDialog(terrainAvailable, doc);
             dialog.ShowDialog();
 
             if (dialog.StartImport)
             {
-                XDocument xDoc = XDocument.Load(dialog.FilePath);
-                XPlanungReader xPlanReader = new XPlanungReader(xDoc);
-                xPlanReader.readData();
-                
-                var xPlanObjx = xPlanReader.XPlanungObjects;
-                var layerNameList = dialog.LayerNamesToImport;
+                var gmlReader = new OGRGMLReader(dialog.FilePath);
+                var geoObjBuilder = new GeoObjectBuilder(doc);
+
+                Log.Information("Starting XPLan import");
+
+                foreach (var layerName in dialog.LayerNamesToImport)
+                {
+                    var layer = gmlReader.getLayerByName(layerName);
+                    var GeoObjs = gmlReader.getGeoObjectsForLayer(layer, dialog.SpatialFilter);
+                    Log.Information(string.Format("Total of {0} features in layer {1}", GeoObjs.Count, layerName));
+                    var fieldList = gmlReader.getFieldNamesForLayer(layer);
+                    geoObjBuilder.buildGeoObjectsFromList(GeoObjs, dialog.Drape, fieldList);
+                    Log.Information(string.Format("Finished importing layer {0}", layerName));
+                }
+
+                gmlReader.destroy();
+
+                //XDocument xDoc = XDocument.Load(dialog.FilePath);
+                //XPlanungReader xPlanReader = new XPlanungReader(xDoc);
+                //xPlanReader.readData();
+
+                //var xPlanObjx = xPlanReader.XPlanungObjects;
+                //var layerNameList = dialog.LayerNamesToImport;
 
                 // https://stackoverflow.com/questions/10745900/filter-a-list-by-another-list-c-sharp :)
                 //filter object list based on usage type string in layer name list
-                List<XPlanungObject> objsToBuild = xPlanObjx.Where(item => layerNameList.Any(category => category.Equals(item.UsageType))).ToList();
+                //List<XPlanungObject> objsToBuild = xPlanObjx.Where(item => layerNameList.Any(category => category.Equals(item.UsageType))).ToList();
 
-                XPlanBuilder xPlanBuilder = new XPlanBuilder(doc);
-                xPlanBuilder.buildRevitObjects(objsToBuild, dialog.Drape);
+                //XPlanBuilder xPlanBuilder = new XPlanBuilder(doc);
+                //xPlanBuilder.buildRevitObjects(objsToBuild, dialog.Drape);
             }
 
             return Result.Succeeded;
