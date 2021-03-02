39fd2bfb77312c4f440ccf7886728de2d10225ba
Marcus Schröder <marcus.schroeder2@htw-dresden.de>
2021-03-02 12:50:27 +0100

    update: store ifc file

--- IFCTerrainGUI/MainWindow.xaml.cs
+++ IFCTerrainGUI/MainWindow.xaml.cs
@@ -20,6 +20,13 @@ using BIMGISInteropLibs.IfcTerrain; //used for JsonSettings, ...
 
 using System.IO; //used for file handling (e.g. open directory)
 
+using IFCTerrainGUI.GUI.ExportSettings; //used for export handling (user controler)
+
+using Newtonsoft.Json; //used for serialize the json file
+
+using Microsoft.Win32;  //file handing (storage location)
+
+using System.ComponentModel; //used for background worker
 
 namespace IFCTerrainGUI
 {
@@ -35,7 +42,11 @@ namespace IFCTerrainGUI
         {
             //all components of the GUI are created (never remove)
             InitializeComponent();
+
+            backgroundWorkerIfc.DoWork += BackgroundWorkerIfc_DoWork;
+            backgroundWorkerIfc.RunWorkerCompleted += BackgroundWorkerIfc_RunWorkerCompleted;
         }
+
 
         /// <summary>
         /// create an instance for Json Settings (getter + setter)
@@ -43,6 +54,9 @@ namespace IFCTerrainGUI
         /// </summary>
         public static JsonSettings jSettings { get; set; } = new JsonSettings();
 
+        /// <summary>
+        /// opens documentation
+        /// </summary>
         private void tbDocumentation_MouseDown(object sender, MouseButtonEventArgs e)
         {
             //direct Link to GITHUB - Repro so it should be accessable for "all"
@@ -51,9 +65,105 @@ namespace IFCTerrainGUI
             System.Diagnostics.Process.Start(docuPath);
         }
 
+        /// <summary>
+        /// Sets the location of the IFC file (via JSON settings)
+        /// </summary>
+        private void btnChooseStorageLocation_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Industry Fundation Classes | *.ifc";
+            if (sfd.ShowDialog() == true)
+            {
+                //gui information
+                MainWindowBib.setTextBoxText(tbIfcDir, sfd.FileName);
+
+                //set filepath to jSettings
+                jSettings.destFileName = sfd.FileName;
+
+                //TODO LOGGING
+            }
+            else
+            {
+                //TODO LOGGING (GUI LOGGING)
+            }
+            return;
+        }
+        /// <summary>
+        /// Start the conversion (based on the settings the user made)
+        /// </summary>
         private void btnStart_Click(object sender, RoutedEventArgs e)
         {
-            //
+            //json settings set 3D to true [TODO]
+            jSettings.is3D = true;
+            //json settings set minDist to 1.0 (default value) [TODO]
+            jSettings.minDist = 1.0;
+
+            //serialize json file
+            try
+            {
+                //get filepath
+                string dirPath = System.IO.Path.GetDirectoryName(jSettings.destFileName);
+                
+                //convert to json object
+                string jExportText = JsonConvert.SerializeObject(jSettings);
+
+                
+                string fileType = jSettings.fileType.ToString();
+                string ifcVersion = jSettings.outIFCType.ToString();
+                string shape = jSettings.surfaceType.ToString();
+                File.WriteAllText(dirPath + @"\config_" + fileType + "_" + ifcVersion + "_" + shape + ".json", jExportText);
+                
+                //TODO Logging
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.Message.ToString() + Environment.NewLine);
+
+                //TODO LOGGING
+            }
+            
+            //lock MainWindow 
+            this.IsEnabled = false;
+
+            //kick off background worker ifc
+            backgroundWorkerIfc.RunWorkerAsync();
         }
+
+        #region background worker
+        /// <summary>
+        /// BackgroundWorker (IFC): used to ...
+        /// </summary>
+        private readonly BackgroundWorker backgroundWorkerIfc = new BackgroundWorker();
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void BackgroundWorkerIfc_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            ConnectionInterface conInt = new ConnectionInterface();
+            conInt.mapProcess(jSettings);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackgroundWorkerIfc_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //release the MainWindow (conversion is completed)
+            this.IsEnabled = true;
+
+            //TODO logging
+        }
+
+
+
+        #endregion background worker
+
+
     }
 }
