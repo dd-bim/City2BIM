6e0a984e3ff846d5586ed47e4f25357fb3476740
Marcus Schröder <marcus.schroeder2@htw-dresden.de>
2021-03-04 08:46:16 +0100

    add uc reading reb

---
+++ IFCTerrainGUI/GUI/REB/ucReadReb.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IFCTerrainGUI.GUI.REB
+{
+    /// <summary>
+    /// Interaction logic for ucReadReb.xaml
+    /// </summary>
+    public partial class ucReadReb : UserControl
+    {
+        public ucReadReb()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void btnReadReb_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void tbRebSelect_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void btnProcessReb_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
