c64ea0ad3b547862119941e1a8fdaaaa1a9ace37
Marcus Schröder <marcus.schroeder@htw-dresden.de>
2021-06-08 09:14:36 +0200

    add first draft for request epsg codes

--- BIMGISInteropLibs/EPSG/Request.cs
+++ BIMGISInteropLibs/EPSG/Request.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using RestSharp; //add lib to request from rest api
+using RestSharp.Authenticators;
+
 namespace BIMGISInteropLibs.EPSG
 {
-    class Request
+    public class EPSGReader
     {
+        /// <summary>
+        /// request class for epsg codes
+        /// </summary>
+        public static void Request(int epsgCode)
+        {
+            //set http client
+            var client = new RestClient("https://apps.epsg.org/api");
+
+            //build request string (generic via espg code)
+            string requestString = "/v1/CoordRefSystem/?keywords=" + epsgCode.ToString();
+
+            //build request as rest request
+            var request = new RestRequest(requestString, DataFormat.Json);
+
+            //send request and get response
+            var response = client.Get(request);
+        }
     }
 }
