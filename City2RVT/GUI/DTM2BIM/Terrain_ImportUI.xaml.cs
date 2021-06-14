662089cfa7ff5fbc4e73bd4c4b1a8dd18ab9f82f
Marcus Schroeder <marcus.schroeder17@outlook.de>
2021-06-14 16:00:21 +0200

    add interface for terrain import

---
+++ City2RVT/GUI/DTM2BIM/Terrain_ImportUI.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace City2RVT.GUI.DTM2BIM
+{
+    /// <summary>
+    /// Interaktionslogik für Terrain_ImportUI.xaml
+    /// </summary>
+    public partial class Terrain_ImportUI : UserControl
+    {
+        public Terrain_ImportUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
