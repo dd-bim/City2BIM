58d455a86c982aef58b15fcdfcbb472f94aedd65
Marcus Schroeder <marcus.schroeder17@outlook.de>
2021-06-16 12:36:24 +0200

    add gui support classes

---
+++ GuiHandler/GuiSupport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows; //used to communicate with main window
+using System.Windows.Controls; //TextBox
+
+namespace GuiHandler
+{
+    public class GuiSupport
+    {
+    }
+}
---
+++ GuiHandler/InitClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BIMGISInteropLibs.IfcTerrain;
+
+namespace GuiHandler
+{
+    public class InitClass
+    {
+        /// <summary>
+        /// initalized config class to config conversion
+        /// </summary>
+        public static JsonSettings config = new JsonSettings();
+    }
+}
