d3362df6ded2fbf7da1dee4b156d959dfc6fe270
Marcus Schroeder <marcus.schroeder17@outlook.de>
2021-06-22 15:33:25 +0200

    add exchange class for DTM2BIM

---
+++ BIMGISInteropLibs/RvtTerrain/Result.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//include for data exchange (revit)
+using C2BPoint = BIMGISInteropLibs.Geometry.C2BPoint;
+
+namespace BIMGISInteropLibs.RvtTerrain
+{
+    /// <summary>
+    /// DTM2BIM exchange class <para/>
+    /// Data exchange between ConnectionInterface & DTM writer
+    /// </summary>
+    public class Result
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<C2BPoint> dtmPoints { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<C2BPoint> terrainFaces { get; set; }
+    }
+}
