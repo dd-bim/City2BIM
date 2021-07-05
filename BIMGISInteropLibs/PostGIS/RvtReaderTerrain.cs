0e663556db3308ec1df2846825a0609712e346db
Marcus Schroeder <marcus.schroeder17@outlook.de>
2021-07-05 15:07:59 +0200

    - add reference to token handler class - add empty class to query postgis data via revit

---
+++ BIMGISInteropLibs/PostGIS/RvtReaderTerrain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RestSharp; //needed for RestClient inheritance
+
+using System.IdentityModel.Tokens.Jwt;
+
+namespace BIMGISInteropLibs.PostGIS
+{
+    public class RvtReaderTerrain : RestClient
+    {
+        
+
+
+    }
+}
