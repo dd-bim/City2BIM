fcf8cfa8f028672378220b08845c03c16dcd096d
Tim Kaiser <tim.kaiser@htw-dresden.de>
2021-07-07 11:15:41 +0200

    dataGridEntry class for overview table

---
+++ City2RVT/ExternalDataCatalog/DataGridEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace City2RVT.ExternalDataCatalog
+{
+    public class DataGridEntry
+    {
+        public string RevitID { get; set; }
+        public string RevitCategory { get; set; }
+        public string IfcClassificationReference { get; set; }
+        public string IfcClassification { get; set; }
+    }
+}
