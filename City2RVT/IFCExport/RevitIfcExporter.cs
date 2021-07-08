c99be9800af4a9b4d10539a7c77a0f97e39168b6
Tim Kaiser <tim.kaiser@htw-dresden.de>
2021-07-08 15:29:46 +0200

    change addIfcClassification to use new schema objects

--- City2RVT/IFCExport/RevitIfcExporter.cs
+++ City2RVT/IFCExport/RevitIfcExporter.cs
@@ -316,7 +316,7 @@ namespace City2RVT.IFCExport
                 return;
             }
 
-            createIfcClassifications(model, doc, ifc2ExternalDataGuidDic);
+            addIfcClassifications(model, doc, ifc2ExternalDataGuidDic);
 
             foreach (KeyValuePair<string,string> entry in ifc2ExternalDataGuidDic)
             {
@@ -355,37 +355,11 @@ namespace City2RVT.IFCExport
                         });
                         pSetRel.RelatedObjects.Add(ifcEntity);
                     }
-
-                    /*
-                    foreach (KeyValuePair<string, string> externalDataPair in dict)
-                    {
-                        //attribute data that is stored as single json string in revit gets deserialized to c# dictionary
-                        var propertyDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(externalDataPair.Value);
-
-                        var pSetRel = model.Instances.New<IfcRelDefinesByProperties>(r =>
-                        {
-                            r.RelatingPropertyDefinition = model.Instances.New<IfcPropertySet>(pSet =>
-                            {
-                                pSet.Name = externalDataPair.Key;
-
-                                foreach (KeyValuePair<string, string> property in propertyDict)
-                                {
-                                    pSet.HasProperties.Add(model.Instances.New<IfcPropertySingleValue>(p =>
-                                    {
-                                        p.Name = property.Key;
-                                        p.NominalValue = new IfcText(property.Value);
-                                    }));
-                                }
-                            });
-                        });
-                        pSetRel.RelatedObjects.Add(ifcEntity);
-                    }
-                    */
                 }
             }
         }
 
-        private void createIfcClassifications(IfcStore model, Document doc, Dictionary<string, string> ifc2ExternalDataGuidDic)
+        private void addIfcClassifications(IfcStore model, Document doc, Dictionary<string, string> ifc2ExternalDataGuidDic)
         {
             var externalDataSchema = utils.getSchemaByName("ExternalDataCatalogSchema");
             List<IfcClassification> classList = new List<IfcClassification>();
@@ -420,25 +394,6 @@ namespace City2RVT.IFCExport
                         }
                     }
                 }
-
-                //var currentClassification = JsonConvert.DeserializeObject<IfcClassification>(revitEntity.Get<string>("ifcClassification"));
-                //classifications.Add(currentClassification);
-
-                /*
-                foreach (var classRef in currentClassification.RefList)
-                {
-                    classRefs.Add(classRef);
-
-                    if (IfcClassRefId2RevitUniqueId.ContainsKey(classRef.ID))
-                    {
-                        IfcClassRefId2RevitUniqueId[classRef.ID].Add(entry.Value);
-                    }
-                    else
-                    {
-                        IfcClassRefId2RevitUniqueId.Add(classRef.ID, new List<string> { entry.Value });
-                    }
-                }
-                */
             }
 
             var uniqueClassifications = classifications.GroupBy(c => c.ID).Select(c => c.First()).ToList();
@@ -470,103 +425,6 @@ namespace City2RVT.IFCExport
                     cls.Source = uniqueClassifications.Where(c => c.ID == entry.Key).First().Source;
                     cls.Location = uniqueClassifications.Where(c => c.ID == entry.Key).First().Location;
                     cls.Edition = uniqueClassifications.Where(c => c.ID == entry.Key).First().Edition;
-                    cls.EditionDate = uniqueClassifications.Where(c => c.ID == entry.Key).First().EditionDate;
-                });
-
-                
-
-                foreach (var classRef in entry.Value)
-                {
-                    var ifcClassRef = model.Instances.New<Xbim.Ifc4.ExternalReferenceResource.IfcClassificationReference>(err =>
-                    {
-                        err.Name = uniqueClassificationReferences.Where(cr => cr.ID == classRef).First().Name;
-                        err.Location = uniqueClassificationReferences.Where(cr => cr.ID == classRef).First().Location;
-                        err.ReferencedSource = topReference;
-                    });
-
-                    var affectedRevitUniqueIds = IfcClassRefId2RevitUniqueId[classRef];
-                    List<Xbim.Ifc4.Kernel.IfcObjectDefinition> affectedIfcObjects = new List<IfcObjectDefinition>();
-
-                    foreach (var element in affectedRevitUniqueIds)
-                    {
-                        Guid cSharpGuid = ExportUtils.GetExportId(doc, doc.GetElement(element).Id);
-                        string ifcGuid = IfcGuid.ToIfcGuid(cSharpGuid);
-                        affectedIfcObjects.Add(model.Instances.FirstOrDefault<IfcObjectDefinition>(o => o.GlobalId == ifcGuid));
-                    }
-
-                    var mapping = model.Instances.New<Xbim.Ifc4.Kernel.IfcRelAssociatesClassification>(relClass =>
-                    {
-                        relClass.RelatingClassification = ifcClassRef;
-                        relClass.RelatedObjects.AddRange(affectedIfcObjects);
-                    });
-                }
-            }
-        }
-
-        private void createIfcClassifications(IfcStore model, Document doc, Dictionary<string, string> ifc2ExternalDataGuidDic)
-        {
-            var externalDataSchema = utils.getSchemaByName("ExternalDataCatalogSchema");
-            List<IfcClassification> classList = new List<IfcClassification>();
-            List<IfcClassificationReference> refClassList = new List<IfcClassificationReference>();
-
-            List<Autodesk.Revit.DB.ExtensibleStorage.Entity> entityList = new List<Entity>();
-            List<IfcClassification> classifications = new List<IfcClassification>();
-            List<IfcClassificationReference> classRefs = new List<IfcClassificationReference>();
-            Dictionary<string, List<string>> IfcClassRefId2RevitUniqueId = new Dictionary<string, List<string>>();
-
-            foreach (KeyValuePair<string, string> entry in ifc2ExternalDataGuidDic)
-            {
-                var revitEntity = doc.GetElement(entry.Value).GetEntity(externalDataSchema);
-                entityList.Add(revitEntity);
-
-                var currentClassification = JsonConvert.DeserializeObject<IfcClassification>(revitEntity.Get<string>("ifcClassification"));
-                classifications.Add(currentClassification);
-
-                foreach (var classRef in currentClassification.RefList)
-                {
-                    classRefs.Add(classRef);
-
-                    if (IfcClassRefId2RevitUniqueId.ContainsKey(classRef.ID))
-                    {
-                        IfcClassRefId2RevitUniqueId[classRef.ID].Add(entry.Value);
-                    }
-                    else
-                    {
-                        IfcClassRefId2RevitUniqueId.Add(classRef.ID, new List<string> { entry.Value });
-                    }
-                }
-            }
-
-            var uniqueClassifications = classifications.GroupBy(c => c.ID).Select(c => c.First()).ToList();
-            var uniqueClassificationReferences = classRefs.GroupBy(cf => cf.ID).Select(cf => cf.First()).ToList();
-
-            Dictionary<string, List<string>> hierarchy = new Dictionary<string, List<string>>();
-
-            foreach (var entry in uniqueClassificationReferences)
-            {
-                var key = entry.ReferencedSource;
-                var value = entry.ID;
-
-                if (hierarchy.ContainsKey(key))
-                {
-                    hierarchy[key].Add(value);
-                }
-                else
-                {
-                    hierarchy.Add(key, new List<string> { value });
-                }
-            }
-
-            //loop through keys -> new ifclcassification for each entry
-            foreach (KeyValuePair<string, List<string>> entry in hierarchy)
-            {
-                var topReference = model.Instances.New<Xbim.Ifc4.ExternalReferenceResource.IfcClassification>(cls =>
-                {
-                    cls.Name = uniqueClassifications.Where(c => c.ID == entry.Key).First().Name;
-                    cls.Source = uniqueClassifications.Where(c => c.ID == entry.Key).First().Source;
-                    cls.Location = uniqueClassifications.Where(c => c.ID == entry.Key).First().Location;
-                    cls.Edition = uniqueClassifications.Where(c => c.ID == entry.Key).First().Edition;
-                    cls.EditionDate = uniqueClassifications.Where(c => c.ID == entry.Key).First().EditionDate;
                     cls.EditionDate = uniqueClassifications.Where(c => c.ID == entry.Key).First().EditionDate;
                 });
 
