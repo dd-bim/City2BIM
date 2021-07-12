c4ab284f27dac244e37e51661b2f77f4214660c9
Marcus Schröder <marcus.schroeder2@htw-dresden.de>
2021-07-12 11:39:12 +0200

    init commit surveyorsplan

---
+++ City2RVT/GUI/Surveyorsplan2BIM/Cmd_Surveyorsplan.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//include Revit API
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace City2RVT.GUI
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class Cmd_Surveyorsplan : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData revit, ref string message, ElementSet elements)
+        {
+            //get current revit document
+            Document doc = revit.Application.ActiveUIDocument.Document;
+
+            //get georef settings based on revit doc
+            Prop_GeoRefSettings.SetInitialSettings(doc);
+
+            //init import ui
+            Surveyorsplan2BIM.Surveyorsplan_ImportUI importUI = new Surveyorsplan2BIM.Surveyorsplan_ImportUI();
+
+            //show main window
+            importUI.ShowDialog();
+
+            if (importUI.startSurvImport)
+            {
+                //TODO
+                return Result.Failed;
+            }
+            else
+            {
+                return Result.Failed;
+            }
+        }
+    }
+}
---
+++ City2RVT/GUI/Surveyorsplan2BIM/Surveyorsplan_ImportUI.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace City2RVT.GUI.Surveyorsplan2BIM
+{
+    /// <summary>
+    /// Interaktionslogik für Surveyorsplan_ImportUI.xaml
+    /// </summary>
+    public partial class Surveyorsplan_ImportUI : Window
+    {
+        #region config
+        /// <summary>
+        /// return to start surv import in cmd 
+        /// </summary>
+        public bool startSurvImport { get { return startImport; } }
+        
+        /// <summary>
+        /// value to be specified that the import should be started
+        /// </summary>
+        private bool startImport { get; set; } = false;
+        #endregion config
+
+        public Surveyorsplan_ImportUI()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
