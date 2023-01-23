ecb071624c656131e0e3e1416686261a1d26dea6
Tim Kaiser <tim.kaiser@htw-dresden.de>
2023-01-23 14:47:31 +0100

    add ifc image to resource and folder

--- DataCatPlugin/ResourceImages.Designer.cs
+++ DataCatPlugin/ResourceImages.Designer.cs
@@ -63,6 +63,16 @@ namespace DataCatPlugin {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap IFC_32px_96dpi {
+            get {
+                object obj = ResourceManager.GetObject("IFC_32px_96dpi", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap loginIcon32 {
             get {
                 object obj = ResourceManager.GetObject("loginIcon32", resourceCulture);
