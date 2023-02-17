165e0c73d531df21f82bc8c08c8b046387503b98
Tim Kaiser <tim.kaiser@htw-dresden.de>
2023-02-17 15:13:52 +0100

    remove data cat duplicated code from citybim

--- CommonRevit/IFC/RevitIfcExporter.cs
+++ CommonRevit/IFC/RevitIfcExporter.cs
@@ -25,10 +25,7 @@ namespace CommonRevit.IFC
 
     public class RevitIfcExporter
     {
-        public enum ExportType
-        {
-            IfcSite, IfcGeographicElement
-        }
+        
         public Document doc { get; protected set; }
 
         private XbimEditorCredentials editor = new XbimEditorCredentials
@@ -60,103 +57,32 @@ namespace CommonRevit.IFC
 
         }
 
-        public void startRevitIfcExport(string outFilePath, ExternalCommandData commandData)
+        public virtual void startRevitIfcExport(string outFilePath, ExternalCommandData commandData)
         {
-            FilteredElementCollector topoCollector = new FilteredElementCollector(this.doc).OfCategory(BuiltInCategory.OST_Topography);
             var view = commandData.Application.ActiveUIDocument.ActiveView;
-            List<ElementId> elementsToHide = topoCollector.Select(element => element.Id).ToList();
 
-            using (TransactionGroup transGroup = new TransactionGroup(doc, "start default Revit export"))
-            {
-                transGroup.Start();
-
-                if (elementsToHide.Count > 0)
-                {
-                    using (Transaction hideTrans = new Transaction(doc, "hide topography elements"))
-                    {
-                        hideTrans.Start();
-
-                        view.HideElements(elementsToHide);
-
-                        hideTrans.Commit();
-                    }
-                }
-
-
-                using (Transaction exportTrans = new Transaction(doc, "default export"))
-                {
-                    exportTrans.Start();
-
-                    IFCExportOptions IFCOptions = new IFCExportOptions();
-                    IFCOptions.FileVersion = IFCVersion.IFC4;
-                    IFCOptions.FilterViewId = view.Id;
-                    IFCOptions.AddOption("ActiveViewId", view.Id.ToString());
-                    IFCOptions.AddOption("ExportVisibleElementsInView", "true");
-                    IFCOptions.AddOption("VisibleElementsOfCurrentView", "true");
-
-                    Log.Information("Startet Revit IFC-Export");
-                    doc.Export(Path.GetDirectoryName(outFilePath), Path.GetFileName(outFilePath), IFCOptions);
-
-
-                    exportTrans.Commit();
-                }
-
-                Log.Information("Finished Revit IFC-Export");
-
-                if (elementsToHide.Count > 0)
-                {
-                    using (Transaction unHideTrans = new Transaction(doc, "unhide topo elements"))
-                    {
-                        unHideTrans.Start();
-                        view.UnhideElements(elementsToHide);
-                        unHideTrans.Commit();
-                    }
-                }
-
-                transGroup.Commit();
-            }
-        }
         
-
-        public void exportModelCurves(IfcStore model)
-        {
-            var elementIdList = new List<ElementId>();
-            var htwSchemaList = SchemaUtils.getHTWSchemas();
-
-            //Filter ModelCurves --> ModelLines and ModelArcs
-            ElementClassFilter modelCurveFilter = new ElementClassFilter(typeof(CurveElement));
-
-            foreach (var schema in htwSchemaList)
+            using (Transaction exportTrans = new Transaction(doc, "default export"))
             {
-                FilteredElementCollector modelCurveCollector = new FilteredElementCollector(this.doc);
-                //Filter elements that have a HTWDD Schema entity associated with
-                ExtensibleStorageFilter storageFilter = new ExtensibleStorageFilter(schema.GUID);
+                exportTrans.Start();
 
-                LogicalAndFilter combinedFilter = new LogicalAndFilter(modelCurveFilter, storageFilter);
+                IFCExportOptions IFCOptions = new IFCExportOptions();
+                IFCOptions.FileVersion = IFCVersion.IFC4;
+                IFCOptions.FilterViewId = view.Id;
+                IFCOptions.AddOption("ActiveViewId", view.Id.ToString());
+                IFCOptions.AddOption("ExportVisibleElementsInView", "true");
+                IFCOptions.AddOption("VisibleElementsOfCurrentView", "true");
 
-                elementIdList.AddRange(modelCurveCollector.WherePasses(combinedFilter).ToElementIds());
+                Log.Information("Startet Revit IFC-Export");
+                doc.Export(Path.GetDirectoryName(outFilePath), Path.GetFileName(outFilePath), IFCOptions);
+
+
+                exportTrans.Commit();
             }
 
-            if (elementIdList.Count > 0)
-            {
-                Log.Information(string.Format("A total of {0} model lines are going to be exported", elementIdList.Count));
-                foreach (var elementId in elementIdList)
-                {
-                    var modelCurve = this.doc.GetElement(elementId) as ModelCurve;
-                    var schemattributes = SchemaUtils.getSchemaAttributesForElement(modelCurve);
-                    var usageType = Schema.Lookup(modelCurve.GetEntitySchemaGuids().First()).SchemaName;
-
-                    if (schemattributes.Count > 0)
-                    {
-                        IfcUtils.addIfcGeographicElementFromModelLine(model, modelCurve, usageType, schemattributes);
-                    }
-                    else
-                    {
-                        IfcUtils.addIfcGeographicElementFromModelLine(model, modelCurve, usageType);
-                    }
-                }
+            Log.Information("Finished Revit IFC-Export");
 
             }
-        }
+
     }
 }
