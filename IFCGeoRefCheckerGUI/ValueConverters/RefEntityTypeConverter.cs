2d3d8035fcdb139cafe1a521f306ba74097e09ef
Tim Kaiser <tim.kaiser@htw-dresden.de>
2023-03-10 13:52:26 +0100

    implement value converters for ref entities

---
+++ IFCGeoRefCheckerGUI/ValueConverters/RefEntityTypeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Xbim.Ifc4.Interfaces;
+
+namespace IFCGeoRefCheckerGUI.ValueConverters
+{
+    class RefEntityTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var product = (IIfcProduct)value;
+            var prodType = product.GetType().Name;
+            return prodType;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
