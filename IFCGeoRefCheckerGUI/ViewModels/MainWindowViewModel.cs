3b080b6b6a81bad9784c127f6beea5aae7f273c2
Tim Kaiser <tim.kaiser@htw-dresden.de>
2023-03-10 13:55:45 +0100

    implement update handling

--- IFCGeoRefCheckerGUI/ViewModels/MainWindowViewModel.cs
+++ IFCGeoRefCheckerGUI/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using IFCGeoRefCheckerGUI.Messaging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace IFCGeoRefCheckerGUI.ViewModels
 {
@@ -16,12 +18,41 @@ namespace IFCGeoRefCheckerGUI.ViewModels
 
         public EventAggregator eventAggregator = new EventAggregator();
 
+        public ICommand OpenUpdateDialogCommand { get; init; }
+
+        public event EventHandler? OpenUpdateDialog;
+
+        List<Subscription>? eventSubscriptions;
+
+        private string? selectedPath;
+        public string? SelectedPath {
+            get => selectedPath;
+            set
+            {
+                if (selectedPath != value)
+                {
+                    selectedPath = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
         public MainWindowViewModel() {
             
             workingDirViewModel = new WorkingDirViewModel(this.eventAggregator);
             filePanelViewModel = new FilePanelViewModel(this.eventAggregator);
             checkViewModel = new CheckViewModel(this.eventAggregator);
 
+            this.eventSubscriptions = new List<Subscription>
+            {
+                eventAggregator.Subscribe<SelectedPathMessageObject>(spmo => this.SelectedPath = spmo.SelectedPath!)
+                //eventAggregator.Subscribe<SelectedWorkingDirMessageObject>(swdmo => NewWorkingDirReceived(swdmo.WorkingDir))
+            };
+
+            this.OpenUpdateDialogCommand = new DelegateCommand((o) =>
+            {
+                this.OpenUpdateDialog?.Invoke(this, EventArgs.Empty);
+            });
         }
     }
 }
