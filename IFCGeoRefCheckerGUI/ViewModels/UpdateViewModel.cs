9ea00f1ebdcbd45df44f8f843ba12466aab11c82
Tim Kaiser <tim.kaiser@htw-dresden.de>
2023-03-14 16:35:29 +0100

    add property to distinguish between ifc versions

--- IFCGeoRefCheckerGUI/ViewModels/UpdateViewModel.cs
+++ IFCGeoRefCheckerGUI/ViewModels/UpdateViewModel.cs
@@ -43,17 +43,56 @@ namespace IFCGeoRefCheckerGUI.ViewModels
             }
         }
 
+        
+        private Level50UI? level50;
+        public Level50UI? Level50
+        {
+            get => level50;
+            set
+            {
+                if (level50 != value)
+                {
+                    level50 = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        private readonly bool isIFC4;
+        public bool IsIFC4
+        {
+            get => isIFC4;
+        }
         public UpdateViewModel(GeoRefChecker geoRefChecker)
         {
             this.geoRefChecker = geoRefChecker;
-            var level10UIs = new List<Level10UI>(geoRefChecker.LoGeoRef10.Count);
+            var Level10UIs = new List<Level10UI>(geoRefChecker.LoGeoRef10.Count);
             foreach (var lvl10 in geoRefChecker.LoGeoRef10)
             {
-                level10UIs.Add(LevelsConverter.convertToLevel10UI(lvl10));
+                Level10UIs.Add(LevelsConverter.convertToLevel10UI(lvl10));
+            }
+
+            this.Level10s = new ObservableCollection<Level10UI>(Level10UIs);
+            //this.Level10s = new ObservableCollection<Level10>(geoRefChecker.LoGeoRef10);
+            this.Level20s = new ObservableCollection<Level20>(geoRefChecker.LoGeoRef20);
+
+            foreach (var lvl50 in geoRefChecker.LoGeoRef50)
+            {
+                if (lvl50.IsFullFilled)
+                {
+                    this.Level50 = LevelsConverter.convertToLevel50UI(lvl50);
+                }
+            }
+            if (this.level50 == null)
+            {
+                this.Level50 = new Level50UI();
             }
             
-            this.Level10s = new ObservableCollection<Level10UI>(level10UIs);
-            this.Level20s = new ObservableCollection<Level20>(geoRefChecker.LoGeoRef20);
+            if (this.geoRefChecker.IFCVersion == Xbim.Common.Step21.XbimSchemaVersion.Ifc4)
+            {
+                this.isIFC4 = true;
+            }
+
         }
 
         public UpdateViewModel()
