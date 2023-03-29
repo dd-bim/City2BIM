207be0f24d8db3c3c3af067d0df450394a3698fe
Tim Kaiser <tim.kaiser@htw-dresden.de>
2023-03-29 15:41:20 +0200

    implement export of custom address from update dialog

--- IFCGeoRefCheckerGUI/ViewModels/UpdateViewModel.cs
+++ IFCGeoRefCheckerGUI/ViewModels/UpdateViewModel.cs
@@ -8,13 +8,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 using IFCGeoRefCheckerGUI.ValueConverters;
+using Xbim.Ifc;
+using System.Data;
+using Xbim.Ifc4.Interfaces;
+using Xbim.Ifc4.MeasureResource;
+using System.ComponentModel;
 
 namespace IFCGeoRefCheckerGUI.ViewModels
 {
     public class UpdateViewModel : BaseViewModel
     {
-        public GeoRefChecker? geoRefChecker;
-        
+        private readonly GeoRefChecker? geoRefChecker;
+        public GeoRefChecker? GeoRefChecker { get { return geoRefChecker; } }
+
+
+        public DelegateCommand? StartExport { get; set; }
+
         private ObservableCollection<Level10UI>? level10s;
         public ObservableCollection<Level10UI>? Level10s
         {
@@ -29,6 +38,7 @@ namespace IFCGeoRefCheckerGUI.ViewModels
             }
         }
         
+
         private ObservableCollection<Level20>? level20s;
         public ObservableCollection<Level20>? Level20s
         {
@@ -42,7 +52,6 @@ namespace IFCGeoRefCheckerGUI.ViewModels
                 }
             }
         }
-
         
         private Level50UI? level50;
         public Level50UI? Level50
@@ -63,6 +72,19 @@ namespace IFCGeoRefCheckerGUI.ViewModels
         {
             get => isIFC4;
         }
+
+        private string? outIfcPath;
+        public string? OutIfcPath
+        {
+            get => outIfcPath;
+            set
+            {
+                if (outIfcPath != value)
+                {
+                    outIfcPath = value;
+                }
+            }
+        }
         public UpdateViewModel(GeoRefChecker geoRefChecker)
         {
             this.geoRefChecker = geoRefChecker;
@@ -73,7 +95,6 @@ namespace IFCGeoRefCheckerGUI.ViewModels
             }
 
             this.Level10s = new ObservableCollection<Level10UI>(Level10UIs);
-            //this.Level10s = new ObservableCollection<Level10>(geoRefChecker.LoGeoRef10);
             this.Level20s = new ObservableCollection<Level20>(geoRefChecker.LoGeoRef20);
 
             foreach (var lvl50 in geoRefChecker.LoGeoRef50)
@@ -93,6 +114,74 @@ namespace IFCGeoRefCheckerGUI.ViewModels
                 this.isIFC4 = true;
             }
 
+            var origPath = this.geoRefChecker.FilePath;
+            this.OutIfcPath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(origPath)!, System.IO.Path.GetFileNameWithoutExtension(origPath)) + "_mod.ifc";
+            this.StartExport = new DelegateCommand((o) => ExecExport(o));
+        }
+
+        private void ExecExport(object o)
+        {
+            using (var model = IfcStore.Open(this.GeoRefChecker!.FilePath))
+            {
+                using (var trans = model.BeginTransaction())
+                {
+                    if (this.level10s != null && this.level10s.Count > 0)
+                    {
+                        foreach (var level10UI in level10s)
+                        {
+                            if (!level10UI.hasData()) { continue; }
+
+                            var entity = model.Instances.FirstOrDefault<IIfcSpatialStructureElement>(elem => elem.GlobalId == level10UI.GUID);
+                            var modelAddress = level10UI.ReferencedEntity == "IfcSite" ? ((IIfcSite)entity).SiteAddress : ((IIfcBuilding)entity).BuildingAddress;
+
+                            if (modelAddress == null)
+                            {
+                                if (model.SchemaVersion == Xbim.Common.Step21.XbimSchemaVersion.Ifc2X3)
+                                {
+                                    modelAddress = model.Instances.New<Xbim.Ifc2x3.ActorResource.IfcPostalAddress>();
+                                }
+                                else
+                                {
+                                    modelAddress = model.Instances.New<Xbim.Ifc4.ActorResource.IfcPostalAddress>();
+                                }
+                            }
+
+                            if (level10UI.ReferencedEntity == "IfcSite")
+                            {
+                                ((IIfcSite)entity).SiteAddress = modelAddress;
+                            }
+                            else
+                            {
+                                ((IIfcBuilding)entity).BuildingAddress = modelAddress;
+                            }
+
+                            modelAddress.Country = level10UI.Country;
+                            modelAddress.Town = level10UI.Town;
+                            modelAddress.Region = level10UI.Region;
+                            modelAddress.PostalCode = level10UI.PostalCode;
+                            if (!string.IsNullOrEmpty(level10UI.AddressLine1)) { modelAddress.AddressLines.Add(level10UI.AddressLine1); }
+                            if (!string.IsNullOrEmpty(level10UI.AddressLine2)) { modelAddress.AddressLines.Add(level10UI.AddressLine2); }
+                        }
+                    }
+
+                    if (this.level20s != null && this.level20s.Count > 0) 
+                    {
+                        foreach (var lvl20UI in level20s)
+                        {
+                            var site = model.Instances.FirstOrDefault<IIfcSite>(s => s.GlobalId == lvl20UI.ReferencedEntity!.GlobalId);
+                            site.RefLatitude = lvl20UI.Latitude != null ? IfcCompoundPlaneAngleMeasure.FromDouble((double)lvl20UI.Latitude) : null;
+                            site.RefLongitude = lvl20UI.Longitude != null ? IfcCompoundPlaneAngleMeasure.FromDouble((double)lvl20UI.Longitude) : null;
+                            site.RefElevation = lvl20UI.Elevation != null ? lvl20UI.Elevation : null;
+                        }
+                    }
+
+                    trans.Commit();
+
+                }
+
+                model.SaveAs(OutIfcPath);
+
+            }
         }
 
         public UpdateViewModel()
