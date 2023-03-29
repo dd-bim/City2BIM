f2c8c7f41e66384158e773eb6328f5bc627974ab
Tim Kaiser <tim.kaiser@htw-dresden.de>
2023-03-29 15:46:28 +0200

    small changes and also binding of elevation property

--- IFCGeoRefCheckerGUI/UpdateGeoRefWindow.xaml.cs
+++ IFCGeoRefCheckerGUI/UpdateGeoRefWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using IFCGeoRefCheckerGUI.ValueConverters;
+using IFCGeoRefCheckerGUI.ViewModels;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,10 +27,50 @@ namespace IFCGeoRefCheckerGUI
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void setOutBtn_Click(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("hier");
+            var vm = (UpdateViewModel)DataContext;
+
+            var outFileName = System.IO.Path.GetFileName(vm.OutIfcPath);
+            var defaultDirectory = System.IO.Path.GetDirectoryName(vm.OutIfcPath);
+
+            var dialog = new SaveFileDialog
+            {
+                Title = "Select location for exported Ifc file",
+                FileName = outFileName,
+                InitialDirectory = defaultDirectory,
+                Filter = "IFC files (*.ifc)|*.ifc|All files (*.*)|*.*"
+            };
+
+            var result = dialog.ShowDialog();
+            
+            if (result == true)
+            {
+                vm.OutIfcPath = dialog.FileName;
+            }
         }
 
+        private void exportBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = (UpdateViewModel)DataContext;
+
+            var selectedPath = vm.OutIfcPath;
+
+            if (System.IO.File.Exists(selectedPath))
+            {
+                var mbr = MessageBox.Show("File already exists!\nDo you want to overwrite it?", "File already exists!", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+                if (mbr == MessageBoxResult.OK)
+                {
+                    var inputIsValid = InputValidator.IsValid(this);
+                    if (!inputIsValid)
+                    {
+                        MessageBox.Show("One or more input fields are invalid. Please correct them!", "Validation errors", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                        return;
+                    }
+                    vm.StartExport!.Execute(null);
+                    this.Close();
+                }
+            }
+        }
     }
 }
